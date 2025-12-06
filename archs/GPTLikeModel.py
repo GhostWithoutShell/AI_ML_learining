7805551c51573d08d47dd9e5d10f6a300fd7d7c8
Danil Payberdin <audiosurffer0@gmail.com>
2025-12-06 03:15:23 +0300

    #59 apply init of model

--- archs/GPTLikeModel.py
+++ archs/GPTLikeModel.py
@@ -4,7 +4,7 @@ import numpy as np
 import re
 import torch
 import math
-
+from model_train_tools import DataBuilderImdb
 
 class AttentionLayer(nn.Module):
     def __init__(self, size_kernel, num_heads, pad_index):
@@ -29,18 +29,19 @@ class AttentionLayer(nn.Module):
         x_v_tr = x_v.transpose(1,2)
         x_q_tr = x_q.transpose(1,2)
         scores = torch.matmul(x_q_tr, x_k.transpose(-1,-2))
-        padding_mask = (input_ids != self.pad_index).unsqueeze(1).unsqueeze(2)
         # mask
-        scores = scores/math.sqrt(self.head_dim)
+        padding_mask = (input_ids != self.pad_index).unsqueeze(1).unsqueeze(2)
         tri_mask = torch.tril(torch.ones((seq_len, seq_len), device=x.device)).unsqueeze(0).unsqueeze(0).bool()
         final_mask = padding_mask & tri_mask
+
+        scores = scores/math.sqrt(self.head_dim)
+        
         attention_scores = scores.masked_fill(final_mask == 0, -1e9)
         attention_scores = torch.softmax(attention_scores, dim=-1)
         result = torch.matmul(attention_scores, x_v_tr)
         result = result.transpose(1,2).contiguous()
         result = result.view(batch_size, seq_len, self.size_kernel)
         x = self.projection(result)
-        #result = scores / torch.sqrt(x_v.transpose(-1,-2)
         return x
 
 class FeedForward(nn.Module):
@@ -82,9 +83,9 @@ class GPTLikeModel(nn.Module):
         self.embedding = nn.Embedding(vocab_size, size_kernel, padding_idx=pad_index)
         self.pos_emb = nn.Embedding(512, size_kernel)
         self.norm = nn.LayerNorm(size_kernel)
-        self.attention_blocks = nn.ModuleList({
+        self.attention_blocks = nn.ModuleList([
             AttentionBlock(size_kernel, num_heads, pad_index) for _ in range(num_layers)
-        })        
+        ])        
         self.drop = nn.Dropout(0.2)
         self.fc_out = nn.Linear(size_kernel, vocab_size, bias=False)
         self.embedding.weight.data =self.embedding.weight
@@ -92,7 +93,6 @@ class GPTLikeModel(nn.Module):
     def forward(self, input_ids):
         batch_size, seq_len = input_ids.size()
         positions = torch.arange(0, seq_len).unsqueeze(0).expand(batch_size, seq_len).to(input_ids.device)
-        self.embedding.weight = self.fc_out.weight
         x = self.embedding(input_ids) + self.pos_emb(positions)
         x = self.norm(x)
         for block in self.attention_blocks:
@@ -100,4 +100,26 @@ class GPTLikeModel(nn.Module):
         logits = self.fc_out(self.drop(x))
         return logits
     
+
+## train_input = input_ids[:, :-1]
+## train_targets = input_ids[:, 1:]
+dt = DataBuilderImdb()
+data = dt.getDataFromCsv("D:\MyFiles\Datasets\IMDB\IMDB Dataset.csv")
+params = {
+    "textsColumn": "review",
+    "labelsColumn": "sentiment"
+}
+data = dt.cleanTextFromTrash(data, params)
+data = dt.applyLabelFix(data, params)
+print(data.head())
+
+num_epochs = 10
+batch_size = 32
+learning_rate = 0.001
+vocab_size = 30000
+
+
+
+model = GPTLikeModel(vocab_size=vocab_size, size_kernel=256, num_heads=8, num_layers=3, pad_index=0)
+
 
