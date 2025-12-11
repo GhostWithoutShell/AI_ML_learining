04b29688572968ac427ab729461140974613eda3
Danil Payberdin <audiosurffer0@gmail.com>
2025-12-11 22:02:50 +0300

    #27 apply selection method and add population generation

--- archs/GA/CartPole.py
+++ archs/GA/CartPole.py
@@ -3,9 +3,16 @@ import pygame
 from gymnasium.utils.play import play
 import torch.nn as nn
 import torch
+import random
+import copy
+
+MUTATION_RATE = 0.01
+MUATATION_STREIGHT = 0.001
+
 
 class DefModel(nn.Module):
     def __init__(self):
+        super().__init__()
         self.lin = nn.Linear(4, 1)
     def forward(self, x):
         return self.lin(x)
@@ -20,21 +27,51 @@ def run_episode(env, model):
     terminated = False
     truncated = False
     while not (terminated or truncated):
-        tensor = torch.tensor(state)
+        tensor = torch.tensor(state, dtype=torch.float32)
+        output = model(tensor)
+        output = output.item()
+        mapping_action = 0
+        if output < 0:
+            mapping_action = 0
+        else:
+            mapping_action = 1
+        state, reward, terminated, truncated, _ = env.step(mapping_action)
+        total_reward += reward
+    return total_reward
+
+def build_pop(pop_size = 100):
+    population = []
+    for i in range(pop_size):
+        population.append(DefModel())
+    return population
+def mutate(individual):
+    individual_ = copy.deepcopy(individual)
+    with torch.no_grad():
         
-def fitness_(input):
-    nul = torch.empty((4,1))
-    return input-nul
+    if random.rand() < MUTATION_RATE:
+        rand_noize = torch.rand_like(individual_.parameters.data) * MUATATION_STREIGHT
+        individual_.model.parameters.data += rand_noize
+    return individual_
+def selection(population, k = 3):
+    new_pop = []
+    max_rew = population.max(lambda x : x.total_reward)
+    population = population.sort(lambda x : x.total_reward)
+    new_pop.append(max_rew)
+    for i in len(population) - 1:
+        #samples = random.sample(population, k)
+        #samples = samples.sort(lambda x : x.total_reward)
+        parent1, parent2 = population.choise(k)[1], population.choise(k)[1]
+        child = crossover(parent1, parent2)
+        child = mutate(child)
+        new_pop.append(child)
+    return new_pop
+        
+
     
 
 env = gym.make("CartPole-v1", render_mode="rgb_array")
 env.reset()
-model = DefModel()
-while True:
-    state = env.state()
-    output = model(state)
-    mapping_t = mapping[output]
-    env.step(mapping_t)
+
 
 
 
