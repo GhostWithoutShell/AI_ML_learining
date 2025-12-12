1ab36e94857e961495b826d832b64cc5f8124804
Danil Payberdin <audiosurffer0@gmail.com>
2025-12-12 21:18:03 +0300

    #27 solving cart pole via GA

--- archs/GA/CartPole.py
+++ archs/GA/CartPole.py
@@ -26,7 +26,9 @@ def run_episode(env, model):
     total_reward = 0
     terminated = False
     truncated = False
-    while not (terminated or truncated):
+    max_steps = 500
+    steps = 0
+    while not (terminated or truncated) and steps < max_steps:
         tensor = torch.tensor(state, dtype=torch.float32)
         output = model(tensor)
         output = output.item()
@@ -37,6 +39,7 @@ def run_episode(env, model):
             mapping_action = 1
         state, reward, terminated, truncated, _ = env.step(mapping_action)
         total_reward += reward
+        steps += 1
     return total_reward
 
 def build_pop(pop_size = 100):
@@ -47,31 +50,73 @@ def build_pop(pop_size = 100):
 def mutate(individual):
     individual_ = copy.deepcopy(individual)
     with torch.no_grad():
-        
-    if random.rand() < MUTATION_RATE:
-        rand_noize = torch.rand_like(individual_.parameters.data) * MUATATION_STREIGHT
-        individual_.model.parameters.data += rand_noize
+        for param in individual_.parameters():
+            
+            if random.random() < MUTATION_RATE:
+                rand_noize = torch.rand_like(param) * MUATATION_STREIGHT
+                param.add_(rand_noize)
     return individual_
 def selection(population, k = 3):
     new_pop = []
-    max_rew = population.max(lambda x : x.total_reward)
-    population = population.sort(lambda x : x.total_reward)
-    new_pop.append(max_rew)
-    for i in len(population) - 1:
+    
+    population.sort(key= lambda x : x.total_reward)
+    top_3 = population[-3:]
+    new_pop.extend([copy.deepcopy(ind) for ind in top_3])
+    top_half = population[len(population)//2:]
+    while len(new_pop) < len(population):
         #samples = random.sample(population, k)
         #samples = samples.sort(lambda x : x.total_reward)
-        parent1, parent2 = population.choise(k)[1], population.choise(k)[1]
-        child = crossover(parent1, parent2)
-        child = mutate(child)
+        
+        parent1 = random.choice(top_half)
+        #child = crossover(parent1, parent2)
+        child = mutate(parent1)
         new_pop.append(child)
     return new_pop
         
 
     
 
-env = gym.make("CartPole-v1", render_mode="rgb_array")
+env = gym.make("CartPole-v1", render_mode="rgb_array", max_episode_steps=10000)
 env.reset()
 
+population = build_pop()
+for i in range(50):
+    for j in range(len(population)):
+        reward = run_episode(env ,population[j])
+        population[j].total_reward = reward
+    new_pop = selection(population=population)
+    best_agent = max(new_pop,key = lambda x : x.total_reward)
+    print("Max reward :", best_agent.total_reward, "Gen :", i+1)
+    if best_agent.total_reward >= 499:
+        print("Solved!")
+        torch.save(best_agent.state_dict(), "cartpole_model.pth")
+        break
+    population = new_pop
+
+env.close()
 
 
 
+best_model = max(population, key=lambda x: x.total_reward)
+
+print("Запуск демонстрации лучшей модели...")
+# Создаем среду с режимом human для отрисовки окна
+show_env = gym.make("CartPole-v1", render_mode="human", max_episode_steps=2000)
+
+state, _ = show_env.reset()
+done = False
+
+while not done:
+    # Обязательно рендерим
+    show_env.render()
+    
+    # Логика выбора действия (как в run_episode)
+    tensor = torch.tensor(state, dtype=torch.float32)
+    with torch.no_grad():
+        output = best_model(tensor)
+    action = 0 if output.item() < 0 else 1
+    
+    state, _, terminated, truncated, _ = show_env.step(action)
+    done = terminated or truncated
+
+show_env.close()
